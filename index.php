40ff615c0a63e9d71266980ffbac83cea284d461
sjsfera <63598661+sjsfera@users.noreply.github.com>
2022-11-12 21:42:11 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang='ru'>
+
+<head>
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+    <link href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQEAYAAABPYyMiAAAABmJLR0T///////8JWPfcAAAACXBIWXMAAABIAAAASABGyWs+AAAAF0lEQVRIx2NgGAWjYBSMglEwCkbBSAcACBAAAeaR9cIAAAAASUVORK5CYII=" rel="icon" type="image/x-icon">
+    <title>Добро пожаловать!123</title>
+    <style>
+
+    </style>
+</head>
+
+<body>
+
+
+
+
+<form action="action.php" method="post">
+ <p>Ваше имя: <input type="text" name="name" /></p>
+ <p>Ваш возраст: <input type="text" name="age" /></p>
+ <p><input type="submit" /></p>
+</form>
+
+Здравствуйте, <?php echo htmlspecialchars($_POST['name']); ?>.
+Вам <?php echo (int)$_POST['age']; ?> лет.
+
+
+</body>
+
+
+
+
+
+
+</html>
